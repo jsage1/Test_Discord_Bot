c84f211dc7b629a96976a94f7687a1754f9fbf4b
jsage1 <jsage1@mail.csuchico.edu>
2020-01-17 14:30:43 -0800

    FIxed ping issue and removed token for privacy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Test_Discord_Bot
-This is my testing space for learning to work with a discord bot
+
+This is my testing space for learning to work with a discord bot.
+
+The token for the discord was removed and put in a seperate function to protect my bot but you can recreate the function with your own token to recreate the bot.
--- discord_test.py
+++ discord_test.py
@@ -2,14 +2,14 @@
 import os
 import discord
 from discord.ext import commands
-
-TOKEN = 'NjY3MjI4ODE2NjUyODk0MjQw.XiCiqA.TyqcocuOMbZ77TDI8I3iE-_FPZo'
-
+from get_token import discord_token
+ 
 client = commands.Bot(command_prefix = '!')
+TOKEN = discord_token()
 
 @client.command()
 async def ping(ctx):
-    await ctx.send('pong (round(client.latency*1000))')
+    await ctx.send(F'pong {round(client.latency*1000)}')
 
 @client.event
 async def on_ready():
@@ -18,4 +18,4 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-client.run(TOKEN)
+client.run(TOKEN, bot = True)
